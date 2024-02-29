09ec469e7875b8c7a419ce245a6cd4bfa8d6be72
Vichnemx <Vichnemx@dukes.jmu.edu>
2024-02-29 15:02:04 -0500

    changed some stuff

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("Default").addEventListener('click', function(){})
+document.getElementById("Dark").addEventListener('click', function(){})
+document.getElementById("Light").addEventListener('click', function(){})
