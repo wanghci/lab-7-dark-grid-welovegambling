e3820269cad7e1bf5374f698206bdbdd66996465
Vichnemx <Vichnemx@dukes.jmu.edu>
2024-02-29 15:13:29 -0500

    eplisey warning

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.getElementById("Default").addEventListener('click', function(){})
-document.getElementById("Dark").addEventListener('click', function(){})
-document.getElementById("Light").addEventListener('click', function(){})
+document.getElementById("Dark").addEventListener('click', function(){document.body.classList.add("light-mode")})
+document.getElementById("Light").addEventListener('click', function(){document.body.classList.add("dark-mode")})
