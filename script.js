74401e037774651ce973c744a9fd0561fd2cda25
Jackson Lofquist <lofquijs@dukes.jmu.edu>
2024-02-29 15:20:23 -0500

    I am him

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+document.getElementById("Default").addEventListener('click', function(event){
+    event.preventDefault();
+    document.body.classList.remove("light-mode", "dark-mode");
+  })
 
-document.getElementById("Default").addEventListener('click', function(){})
-document.getElementById("Dark").addEventListener('click', function(){document.body.classList.add("light-mode")})
-document.getElementById("Light").addEventListener('click', function(){document.body.classList.add("dark-mode")})
+  document.getElementById("Dark").addEventListener('click', function(event){
+    event.preventDefault();
+    document.body.classList.remove("light-mode");
+    document.body.classList.add("dark-mode");
+  })
+
+  document.getElementById("Light").addEventListener('click', function(event){
+    event.preventDefault();
+    document.body.classList.remove("dark-mode");
+    document.body.classList.add("light-mode");
+  })
+  
